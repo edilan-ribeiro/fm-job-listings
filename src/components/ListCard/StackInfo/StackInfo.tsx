5ad5a40407212392eb92400e1ea5c881189afbb4
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-12-02 22:08:53 -0300

    Added click function

--- src/components/ListCard/StackInfo/StackInfo.tsx
+++ src/components/ListCard/StackInfo/StackInfo.tsx
@@ -2,14 +2,15 @@ import styles from './StackInfoStyles.module.scss'
 
 interface Stack {
 	stack: string[]
+	click: (stackInfo: string) => void
 }
 
-export const StackInfo = ({ stack }: Stack) => {
+export const StackInfo = ({ stack, click }: Stack) => {
 	return (
 		<ul className={styles.stackInfo}>
 			{stack.map((stackInfo, index) => (
 				<li key={index}>
-					<button>{stackInfo}</button>
+					<button onClick={() => click(stackInfo)}>{stackInfo}</button>
 				</li>
 			))}
 		</ul>
