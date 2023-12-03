2d4a67455cc77dda81b9873e68abde45b2bfb03f
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-12-02 22:08:18 -0300

    Updated main job list

--- src/components/JobList/JobList.tsx
+++ src/components/JobList/JobList.tsx
@@ -2,28 +2,47 @@ import styles from './JobListStyles.module.scss'
 import jobsData from '../../data/data.json'
 import { groupStack } from '../../utils/groupStack'
 import { Cards } from './Cards'
+import { ListFilter } from '../ListFilter/ListFilter'
+import { useState } from 'react'
 
 export const JobList = () => {
+	const [selectedStack, setSelectedStack] = useState<string[]>([])
+
+	function addStackToSelected(stackInfo :string) {
+		if (!selectedStack.includes(stackInfo)) {
+			setSelectedStack((prevState) => [...prevState, stackInfo])
+		}
+	}
+
 	return (
-		<ul className={styles.jobList}>
-			{jobsData.map((jobData, index) => (
-				<Cards.CardList key={jobData.id}>
-					<Cards.Info.JobInfo
-						img={jobData.logo}
-						company={jobData.company}
-						recent={jobData.new}
-						featured={jobData.featured}
-						position={jobData.position}
-					>
-						<Cards.Info.JobTimeInfo
-							days={jobData.postedAt}
-							contractType={jobData.contract}
-							place={jobData.location}
+		<>
+			<ListFilter
+				selectedStack={selectedStack}
+				clearFilter={() => setSelectedStack([])}
+			/>
+			<ul className={styles.jobList}>
+				{jobsData.map((jobData, index) => (
+					<Cards.CardList key={jobData.id}>
+						<Cards.Info.JobInfo
+							img={jobData.logo}
+							company={jobData.company}
+							recent={jobData.new}
+							featured={jobData.featured}
+							position={jobData.position}
+						>
+							<Cards.Info.JobTimeInfo
+								days={jobData.postedAt}
+								contractType={jobData.contract}
+								place={jobData.location}
+							/>
+						</Cards.Info.JobInfo>
+						<Cards.Stack
+							stack={groupStack(jobsData, index)}
+							click={addStackToSelected}
 						/>
-					</Cards.Info.JobInfo>
-					<Cards.Stack stack={groupStack(jobsData, index)} />
-				</Cards.CardList>
-			))}
-		</ul>
+					</Cards.CardList>
+				))}
+			</ul>
+		</>
 	)
 }
