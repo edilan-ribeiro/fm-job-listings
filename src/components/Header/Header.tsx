3278e32b5984fb29dc9db852c862fc18686d3422
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-12-03 19:27:58 -0300

    Simplified component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,5 @@
-import styles from "./Header.module.scss"
+import styles from './Header.module.scss'
 
 export const Header = () => {
-    return (
-        <header className={styles.header}>
-
-        </header>
-    )
+	return <header className={styles.header} />
 }
