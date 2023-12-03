2c144938fa14901e3897e9df5c8093e35663778b
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-12-02 22:07:45 -0300

    Created Filter List

--- src/components/ListFilter/ListFilter.tsx
+++ src/components/ListFilter/ListFilter.tsx
@@ -1,3 +1,25 @@
-export const ListFilter = () => {
-    return
+import styles from './ListFilterStyles.module.scss'
+import iconRemove from '../../../public/images/icon-remove.svg'
+
+interface listFilter {
+	selectedStack: string[]
+	clearFilter: React.MouseEventHandler
+}
+
+export const ListFilter = ({ selectedStack, clearFilter }: listFilter) => {
+	return (
+		<div className={styles.listFilter}>
+			<ul className={styles.selectedStack}>
+				{selectedStack.map((selectedStack, index) => (
+					<li key={index}>
+						<p>{selectedStack}</p>
+						<button onClick={clearFilter} className={styles.removeFilterBtn} title={`Removes ${selectedStack} from filter`}>
+							<img src={iconRemove} alt="icon from remove filter button"/>
+						</button>
+					</li>
+				))}
+			</ul>
+			<button onClick={clearFilter} className={styles.clearBtn}>Clear</button>
+		</div>
+	)
 }
