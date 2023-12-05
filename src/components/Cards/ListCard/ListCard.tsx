11efce5503f2251e55d12a873fe62d78119e882b
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-12-04 22:22:19 -0300

    Small animation update

--- src/components/Cards/ListCard/ListCard.tsx
+++ src/components/Cards/ListCard/ListCard.tsx
@@ -5,8 +5,8 @@ import { motion } from 'framer-motion'
 export const ListCard = ({ children, isrecent }: CardItems) => {
 	return (
 		<motion.li
-			initial={{ opacity: 0, x: -5 }}
-			animate={{ opacity: 1, x:5 }}
+			initial={{ opacity: 0, x: -10 }}
+			animate={{ opacity: 1, x:0 }}
 			transition={{ ease: 'easeInOut', duration: 0.6 }}
 			className={styles.listCards}
 			data-isrecent={isrecent}
